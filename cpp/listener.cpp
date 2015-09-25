a9985be90350e756577cb43807f354652a0e5a0b
M Jordan Stanway <m.j.stanway@alum.mit.edu>
2015-09-25 13:18:44 -0700

    updated listener example using static member function as handler and accepting command-line args for channel and provider

--- cpp/listener.cpp
+++ cpp/listener.cpp
@@ -10,33 +10,43 @@
 
 #include "process_management/syslog_t.hpp"
 
-
-class Handler
+class LCMSyslogListener
 {
     public:
-        ~Handler() {}
+        LCMSyslogListener(const std::string provider= "") :
+            lcm(lcm::LCM(provider))
+        {
+            if(!lcm.good()) std::cout << "LCM would not initialize.";
+        }
 
-        void printSyslogMessage(const lcm::ReceiveBuffer* rbuf,
+        ~LCMSyslogListener() {}
+
+        static void printSyslogMessage(const lcm::ReceiveBuffer* rbuf,
                 const std::string& channel,
-                const process_management::syslog_t* msg)
+                const process_management::syslog_t* msg,
+                LCMSyslogListener*)
         {
             std::cout << epoch_usec_to_iso8601(msg->epoch_usec)
                 << std::left << std::setw(13)
                 << " [" + channel.substr(channel.rfind('.') + 1) + "]"
                 << msg->text << std::endl;
         }
+
+        void listen(const std::string channel = "syslog.*")
+        {
+            lcm.subscribeFunction(channel, this->printSyslogMessage, this);
+            while(0 == lcm.handle());
+        }
+
+    private:
+        lcm::LCM lcm;
 };
 
-// TODO: Accept command line args to filter levels, etc.
 int main(int argc, char ** argv)
 {
-    lcm::LCM lcm;
-    if(!lcm.good()) return 1;
-
-    Handler h;
-    lcm.subscribe("syslog.*", &Handler::printSyslogMessage, &h);
-
-    while(0 == lcm.handle());
-
+    std::string channel = argc > 1 ? argv[1] : "syslog.*";
+    std::string provider = argc > 2 ? argv[2] : "";
+    LCMSyslogListener listener(provider);
+    listener.listen(channel);
     return 0;
 }
