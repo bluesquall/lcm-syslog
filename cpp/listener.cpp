4c9639d8df4bf36225fb221f1153850e6f105d90
M Jordan Stanway <m.j.stanway@alum.mit.edu>
2015-09-24 20:59:26 -0700

    compactify listener handler

--- cpp/listener.cpp
+++ cpp/listener.cpp
@@ -1,12 +1,12 @@
 // listener.cpp
 //
 
-#include <chrono>
-#include <ctime>
 #include <iomanip>
 #include <iostream>
 
 #include <lcm/lcm-cpp.hpp>
+
+#include "util.hpp"
 
 #include "process_management/syslog_t.hpp"
 
@@ -20,15 +20,10 @@ class Handler
                 const std::string& channel,
                 const process_management::syslog_t* msg)
         {
-            std::chrono::microseconds epoch_usec(msg->timestamp);
-            std::chrono::system_clock::time_point tp(epoch_usec);
-            std::time_t tt = std::chrono::system_clock::to_time_t(tp);
-            std::tm tm(*std::gmtime(&tt));
-            // std::size_t p = channel.rfind('.');
-            // std::string level = channel.substr(p);
-            std::string level = channel.substr(7);
-            std::cout<< std::put_time(&tm, "%F %T") <<" ["<< level << "] " << msg->text <<std::endl;
-            //std::cout<< tm.tm_year+1900 <<'-'<< tm.tm_mon+1 <<'-'<< tm.tm_mday <<' '<< tm.tm_hour <<':'<< tm.tm_min <<':'<< tm.tm_sec <<'.'<< "xxx" <<" ["<< level << "] " << msg->text <<std::endl;
+            std::cout << epoch_usec_to_iso8601(msg->timestamp)
+                << std::left << std::setw(13)
+                << " [" + channel.substr(channel.rfind('.') + 1) + "]"
+                << msg->text << std::endl;
         }
 };
 
--- cpp/util.hpp
+++ cpp/util.hpp
@@ -3,8 +3,9 @@
 #ifndef UTIL_HPP
 #define UTIL_HPP
 
-#include <cstdint>
 #include <chrono>
+#include <cstdint>
+#include <ctime>
 
 /* convenience function for epoch seconds */
 inline int64_t epoch_sec(void){
@@ -24,6 +25,21 @@ inline int64_t epoch_usec(void){
 /* convenience function for epoch nanoseconds */
 inline int64_t epoch_nsec(void){
     return std::chrono::duration_cast<std::chrono::nanoseconds>(std::chrono::system_clock::now().time_since_epoch()).count();
+}
+
+inline std::chrono::system_clock::time_point epoch_usec_to_time_point(const int64_t us){
+    return std::chrono::system_clock::time_point(std::chrono::microseconds(us));
+}
+
+inline std::time_t epoch_usec_to_time_t(const int64_t us){
+    return std::chrono::system_clock::to_time_t(std::chrono::system_clock::time_point(std::chrono::microseconds(us)));
+}
+
+inline std::string epoch_usec_to_iso8601(const int64_t us){
+    char iso8601[21];
+    std::time_t t = epoch_usec_to_time_t(us);
+    std::strftime(iso8601, 21, "%F %T.", std::gmtime(&t));
+    return std::string(iso8601) + std::to_string(us % 1000000);
 }
 
 #endif // UTIL_HPP
