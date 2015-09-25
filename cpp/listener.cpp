8be8d6f27a00261b29aa9e845e48bb6d236b944e
M Jordan Stanway <m.j.stanway@alum.mit.edu>
2015-09-25 13:49:07 -0700

    add complex listener example with one LCM instance shared between multiple objects, and an echo handler

--- cpp/listener.cpp
+++ cpp/listener.cpp
@@ -13,18 +13,15 @@
 class LCMSyslogListener
 {
     public:
-        LCMSyslogListener(const std::string provider= "") :
-            lcm(lcm::LCM(provider))
-        {
-            if(!lcm.good()) std::cout << "LCM would not initialize.";
-        }
+        LCMSyslogListener(lcm::LCM* lcm_instance) :
+            lcm(lcm_instance)
+        {}
 
         ~LCMSyslogListener() {}
 
-        static void printSyslogMessage(const lcm::ReceiveBuffer* rbuf,
+        void out(const lcm::ReceiveBuffer* rbuf,
                 const std::string& channel,
-                const process_management::syslog_t* msg,
-                LCMSyslogListener*)
+                const process_management::syslog_t* msg)
         {
             std::cout << epoch_usec_to_iso8601(msg->epoch_usec)
                 << std::left << std::setw(13)
@@ -32,21 +29,46 @@ class LCMSyslogListener
                 << msg->text << std::endl;
         }
 
-        void listen(const std::string channel = "syslog.*")
+        void err(const lcm::ReceiveBuffer* rbuf,
+                const std::string& channel,
+                const process_management::syslog_t* msg)
         {
-            lcm.subscribeFunction(channel, this->printSyslogMessage, this);
-            while(0 == lcm.handle());
+            std::cerr << epoch_usec_to_iso8601(msg->epoch_usec)
+                << std::left << std::setw(13)
+                << " [" + channel.substr(channel.rfind('.') + 1) + "]"
+                << msg->text << std::endl;
         }
 
-    private:
-        lcm::LCM lcm;
+
+        void echo(const lcm::ReceiveBuffer* rbuf,
+                const std::string& channel,
+                const process_management::syslog_t* msg)
+        {
+            lcm->publish("echo." + channel, msg);
+        }
+
+        lcm::LCM* lcm;
 };
 
 int main(int argc, char ** argv)
 {
-    std::string channel = argc > 1 ? argv[1] : "syslog.*";
+    std::string prefix = argc > 1 ? argv[1] : "syslog";
     std::string provider = argc > 2 ? argv[2] : "";
-    LCMSyslogListener listener(provider);
-    listener.listen(channel);
+
+    lcm::LCM lcm(provider);
+
+    // XXX Is it a bad idea to share an LCM instance between two objects?
+    //     (This is admittedly a contrived example).
+    LCMSyslogListener listener(&lcm);
+    LCMSyslogListener echoer(&lcm);
+
+    lcm.subscribe(prefix + ".IMPORTANT", &LCMSyslogListener::out, &listener);
+    lcm.subscribe(prefix + ".INFO", &LCMSyslogListener::out, &listener);
+    lcm.subscribe(prefix + ".CRITICAL", &LCMSyslogListener::err, &listener);
+    lcm.subscribe(prefix + ".ERROR", &LCMSyslogListener::err, &listener);
+    lcm.subscribe(prefix + ".WARNING", &LCMSyslogListener::err, &listener);
+    lcm.subscribe(prefix + ".DEBUG", &LCMSyslogListener::echo, &echoer);
+
+    while(0 == lcm.handle());
     return 0;
 }
