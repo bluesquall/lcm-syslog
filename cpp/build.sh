f1451705d6ef5b3caba88a5bf139ca8c946e5a9b
M Jordan Stanway <m.j.stanway@alum.mit.edu>
2015-09-25 11:08:32 -0700

    clean the existing lcm-generated files before generating them again (just in case)

--- cpp/build.sh
+++ cpp/build.sh
@@ -1,4 +1,5 @@
 #!/bin/bash
+rm -rf ./process-management
 lcm-gen --cpp ../process-management-lcmtypes/*.lcm
 g++ -std=gnu++11 -I /usr/local/include -I . -L/usr/local/lib -llcm lcm-syslog.cpp test.cpp -o test
 g++ -std=gnu++11 -I /usr/local/include -I . -L/usr/local/lib -llcm listener.cpp -o listener
