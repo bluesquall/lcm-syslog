a786b669cece5a03b96b8f6dd86cbca534945779
M Jordan Stanway <m.j.stanway@alum.mit.edu>
2015-11-16 16:20:34 -0800

    update build script with consistent path to generated lcmtypes

--- cpp/build.sh
+++ cpp/build.sh
@@ -1,6 +1,5 @@
 #!/bin/bash
-rm -rf ./process-management
-lcm-gen --cpp ../process-management-lcmtypes/*.lcm
-g++ -std=gnu++11 -I /usr/local/include -I . -L/usr/local/lib -llcm lcm-syslog.cpp test.cpp -o test
-g++ -std=gnu++11 -I /usr/local/include -I . -L/usr/local/lib -llcm listener.cpp -o listener
-# TODO: figure out why I need "-I ." here...
+rm -rf lcmtypes
+lcm-gen --cpp --cpp-hpath lcmtypes ../process-management-lcmtypes/*.lcm
+g++ -std=gnu++11 -I /usr/local/include -I lcmtypes -L/usr/local/lib -llcm lcm-syslog.cpp test.cpp -o test
+g++ -std=gnu++11 -I /usr/local/include -I lcmtypes -L/usr/local/lib -llcm listener.cpp -o listener
