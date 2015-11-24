588c01460241e46078625d8cb25347a6431b5875
M Jordan Stanway <m.j.stanway@alum.mit.edu>
2015-11-24 12:21:27 -0800

    update helper script to run LCM spy

--- java/run-lcm-spy.sh
+++ java/run-lcm-spy.sh
@@ -1,7 +1,9 @@
 #!/bin/bash
-JPATH=./lcmtypes
-rm $JPATH/*/*.java
-lcm-gen --java --jpath=$JPATH ../process-management-lcmtypes/*.lcm
-javac $JPATH/*/*.java
-CLASSPATH=$JPATH lcm-spy
+LPATH=${1:-../process-management-lcmtypes}
+JPATH=${2:-/tmp/lcmtypes}
 
+find $JPATH -type f \( -name "*.java" -o -name "*.class" \) -delete
+find $LPATH -type f -name "*.lcm" -exec lcm-gen --java --jpath $JPATH {} \;
+find $JPATH -type f -name "*.java" -exec javac {} \;
+
+CLASSPATH=$JPATH lcm-spy
