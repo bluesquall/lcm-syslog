ea1d892e497db884a9ebf25436ca929651b42253
M Jordan Stanway <m.j.stanway@alum.mit.edu>
2015-11-24 13:06:14 -0800

    add JPATH to CLASSPATH for javac call
    You need this when types have elements made of otehr types.

--- java/run-lcm-spy.sh
+++ java/run-lcm-spy.sh
@@ -4,6 +4,6 @@ JPATH=${2:-/tmp/lcmtypes}
 
 find $JPATH -type f \( -name "*.java" -o -name "*.class" \) -delete
 find $LPATH -type f -name "*.lcm" -exec lcm-gen --java --jpath $JPATH {} \;
-find $JPATH -type f -name "*.java" -exec javac {} \;
+find $JPATH -type f -name "*.java" -exec javac -cp $JPATH {} \;
 
 CLASSPATH=$JPATH lcm-spy
