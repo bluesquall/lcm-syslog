3fc49bbacb05838de1b322c7de0882ac958a9017
M Jordan Stanway <m.j.stanway@alum.mit.edu>
2016-02-17 11:09:41 -0800

    Remove old build script

--- cpp/build.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/bash
-rm -rf lcmtypes
-lcm-gen --cpp --cpp-hpath lcmtypes ../process-management-lcmtypes/*.lcm
-g++ -std=gnu++11 -I /usr/local/include -I lcmtypes -L/usr/local/lib -llcm lcm-syslog.cpp test.cpp -o test
-g++ -std=gnu++11 -I /usr/local/include -I lcmtypes -L/usr/local/lib -llcm listener.cpp -o listener
